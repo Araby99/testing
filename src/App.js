4e4d07c912d513c777c625a3df763bd9719ebcd9
Ahmed <ahmedaraby01023980886@gmail.com>
2021-10-20 13:20:48 +0200

    V1.1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './App.css';
 
 function App() {
@@ -7,27 +8,9 @@ function App() {
   const [num, setNum] = useState(1)
   const [force, setForce] = useState(forceData)
   const [resultant, setResultant] = useState(forceResultant)
-  const add = event => {
-    event.preventDefault()
-    if (event.target[0].value === "" || event.target[1].value === "") {
-      alert("Write a value dumb")
-    } else {
-      do {
-        event.target[1].value -= 360
-      } while (event.target[1].value > 360);
-      const newForce = {
-        amount: parseInt(event.target[0].value),
-        angle: parseInt(event.target[1].value),
-        name: "F" + num
-      }
-      setNum(num + 1)
-      setForce(force => [...force, newForce])
-    }
-    setResultant([])
-  }
+
   const getResultant = () => {
     let t = 0, b = 0, l = 0, r = 0;
-    console.log(r, t, l, b);
     force.map((force) => {
       const { amount, angle } = force
       if (angle < 90) {
@@ -79,21 +62,39 @@ function App() {
       return (r, t, l, b)
     })
   }
+  const add = event => {
+    event.preventDefault()
+    if (event.target[0].value === "" || event.target[1].value === "") {
+      alert("Write a value dumb")
+    } else {
+      while (event.target[1].value > 360) {
+        event.target[1].value -= 360
+      }
+      const newForce = {
+        amount: parseInt(event.target[0].value),
+        angle: parseInt(event.target[1].value),
+        name: "F" + num
+      }
+      setNum(num + 1)
+      setForce(force => [...force, newForce])
+    }
+    setResultant([])
+  }
   const remove = e => {
     const name = e.target.getAttribute("name")
     setForce(force.filter(item => item.name !== name))
-    console.log(force);
+    setResultant([])
   }
   return (
     <div className="App">
       <div className="add">
         <form onSubmit={add}>
-          <input type="number" name="amount" placeholder="Amount" id="amount" />
-          <input type="number" name="angle" placeholder="Angle" id="angle" />
-          <button type="submit">Add</button>
+          <input type="number" className="form-control" name="amount" placeholder="Amount" id="amount" />
+          <input type="number" className="form-control" name="angle" placeholder="Angle" id="angle" />
+          <button type="submit" className="btn btn-primary">Add</button>
         </form>
 
-        <button onClick={getResultant}>Get Resultant</button>
+        <button onClick={getResultant} className="get btn btn-primary">Get Resultant</button>
       </div>
       <div className="information">
         <div className="force-info">
@@ -102,10 +103,12 @@ function App() {
               const { amount, angle, name } = force;
               return (
                 <div key={index}>
-                  <span>Name : F{index + 1} </span>
-                  <span>Amount: {amount} </span>
-                  <span>Angle: {angle} </span>
-                  <button onClick={remove} name={name}>Remove</button>
+                  <div>
+                    <span>Name : F{index + 1} </span>
+                    <span>Amount: {amount} </span>
+                    <span>Angle: {angle} °</span>
+                  </div>
+                  <button className="btn btn-danger" onClick={remove} name={name}>Remove</button>
                 </div>
               )
             })
@@ -117,7 +120,7 @@ function App() {
               const { amount, angle } = res;
               return (
                 <div key={index}>
-                  <span>Res is {amount} and his angle is {angle}</span>
+                  <span>Res is {amount} and his angle is {angle}°</span>
                 </div>
               )
             })
@@ -128,14 +131,14 @@ function App() {
         {
           force.map((force, index) => {
             return (
-              <div className="force" key={index} style={{ transform: `rotate(${90 - force.angle}deg)` }}></div>
+              <div className="force qs" key={index} style={{ transform: `rotate(${90 - force.angle}deg)` }}><span className="popover above" style={{ transform: `rotate(${force.angle - 90}deg)` }} >Name: {force.name} <br /> Amount: {force.amount} <br /> Angle: {force.angle}°</span></div>
             )
           })
         }
         {
           resultant.map((force, index) => {
             return (
-              <div className="force resultant" key={index} style={{ transform: `rotate(${90 - force.angle}deg)` }}></div>
+              <div className="force qs resultant" key={index} style={{ transform: `rotate(${90 - force.angle}deg)` }}></div>
             )
           })
         }
